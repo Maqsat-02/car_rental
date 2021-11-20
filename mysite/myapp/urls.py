413e342400f0c8b52426c50f1c1cc399f4dbed39
Maqsat-02 <mvqcvt@gmail.com>
2021-11-20 22:04:51 +0600

    commit

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 
+from .views import send_gmail
+
 urlpatterns = [
     path('', views.main, name='main'),
     path('about/', views.about, name='about'),
     path('catalog/', views.catalog, name='catalog'),
     path('contacts/', views.contacts, name='contacts'),
+    path('send_mail/', send_gmail, name="send_mail"),
     path('sign/', views.loginPage, name='sign'),
     path('registration/', views.registerPage, name='registration'),
+    path('logout/',views.logoutUser,name = 'logout')
 ]
